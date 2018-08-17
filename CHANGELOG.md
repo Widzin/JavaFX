be3946ad0fa48f3e96c8b11015823afda1e40949
= <a.widzinski@gmail.com>
2018-08-17 21:58:07 +0200

    put small layouts in one bigger

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 2018-08-17
+```
+Display many layouts in bigger one.
+```
+
 # 2018-07-08
 ```
 Created empty project
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,7 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,8 +17,23 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Hello World");
 
-        StackPane layout = new StackPane();
-        Scene scene = new Scene(layout, 300, 275);
+        HBox topMenu = new HBox();
+        Button fileBtn = new Button("File");
+        Button editBtn = new Button("Edit");
+        Button viewBtn = new Button("View");
+        topMenu.getChildren().addAll(fileBtn, editBtn, viewBtn);
+
+        VBox leftMenu = new VBox();
+        Button dBtn = new Button("D");
+        Button eBtn = new Button("E");
+        Button fBtn = new Button("F");
+        leftMenu.getChildren().addAll(dBtn, eBtn, fBtn);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(leftMenu);
+
+        Scene scene = new Scene(borderPane, 300, 275);
         window.setScene(scene);
         window.show();
     }
