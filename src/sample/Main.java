6468b05fad911225f7f9104a21997957720ea6b9
= <a.widzinski@gmail.com>
2018-08-17 21:51:11 +0200

    prepare project for another lesson

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,23 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private Stage window;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        window = primaryStage;
+        window.setTitle("Hello World");
+
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 300, 275);
+        window.setScene(scene);
+        window.show();
     }
 
 
