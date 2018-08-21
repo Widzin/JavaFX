a4db6a3c7b79f9f6c8016f3b16ff197c785b4c07
= <a.widzinski@gmail.com>
2018-08-21 22:55:21 +0200

    create listView

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,7 +19,7 @@ public class Main extends Application {
     private Stage window;
     private Scene scene;
     private Button button;
-
+    private ListView<String> listView;
 
     @Override
     public void start(Stage primaryStage) {
@@ -26,12 +28,25 @@ public class Main extends Application {
 
         button = new Button("Click me");
 
+        listView = new ListView<>();
+        listView.getItems().addAll("Potato", "Corn", "Tomatoe", "Strawberry", "Banana");
+        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+        button.setOnAction(event -> buttonClicked());
+
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll( button);
+        layout.getChildren().addAll(listView, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
+    }
+
+    private void buttonClicked() {
+        StringBuilder message = new StringBuilder();
+        ObservableList<String> food = listView.getSelectionModel().getSelectedItems();
+        food.forEach(s -> message.append(s).append("\n"));
+        System.out.println(message.toString());
     }
 }
