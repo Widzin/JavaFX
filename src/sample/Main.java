1573623a61d4aa115960461ccfad357838ffe232
= <a.widzinski@gmail.com>
2018-08-21 20:41:12 +0200

    added gridpane with simplest form

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,12 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,11 +15,33 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
         window.setTitle("Hello World");
 
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
 
+        Label nameLabel = new Label("Username: ");
+        GridPane.setConstraints(nameLabel, 0, 0);
 
+        TextField nameInput = new TextField("Widzin");
+        GridPane.setConstraints(nameInput, 1, 0);
 
+        Label passLabel = new Label("Password: ");
+        GridPane.setConstraints(passLabel, 0, 1);
+
+        TextField passInput = new TextField();
+        passInput.setPromptText("password");
+        GridPane.setConstraints(passInput, 1, 1);
+
+        Button loginBtn = new Button("Log In");
+        GridPane.setConstraints(loginBtn, 1, 2);
+
+        gridPane.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginBtn);
+        Scene scene = new Scene(gridPane, 300, 250);
+        window.setScene(scene);
         window.show();
     }
 
