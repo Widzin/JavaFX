5444ce0402f733fd42aaca211caed83cdfc71bf1
= <a.widzinski@gmail.com>
2018-08-21 21:02:10 +0200

    prepare project for another lesson

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,27 +1,35 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Button button;
+    private Stage window;
+    private Scene scene;
+    private Button button;
+
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World");
+        window = primaryStage;
+        window.setTitle("Hello World");
 
+        TextField input = new TextField();
         button = new Button("Click me");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        VBox layout = new VBox(10);
+        layout.setPadding(new Insets(20));
+        layout.getChildren().addAll(input, button);
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        scene = new Scene(layout, 300, 250);
+        window.setScene(scene);
+        window.show();
     }
 
 
