99b9c806ac5e5aac0b714a4a96a7572f09a79f96
= <a.widzinski@gmail.com>
2018-08-21 21:32:30 +0200

    add choiceBox and print selected one

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This is my project for training writing JavaFX applications.
-### This project display simple window with button.
+### This project display simple window with choiceBox.
 
 ### This project is based on tutorial from youtube, link below:
 https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -24,11 +24,16 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Hello World");
 
+        ChoiceBox<String> meets = new ChoiceBox<>();
+        meets.getItems().addAll("Bacon", "Chicken", "Ham");
+        meets.setValue("Bacon");
+
         button = new Button("Click me");
+        button.setOnAction(event -> System.out.println(meets.getValue()));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll( button);
+        layout.getChildren().addAll(meets, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
