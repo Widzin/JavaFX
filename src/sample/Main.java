9061e4602e82daaf4a749961a233ffdde07eda93
= <a.widzinski@gmail.com>
2018-08-21 21:18:18 +0200

    added checkboxes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,11 +4,15 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     private Stage window;
     private Scene scene;
@@ -20,20 +24,17 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("Hello World");
 
-        TextField input = new TextField();
-        button = new Button("Click me");
+        button = new Button("Order now");
+
+        CheckBox chickenBox = new CheckBox("Chicken");
+        CheckBox baconBox = new CheckBox("Bacon");
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll(input, button);
+        layout.getChildren().addAll(chickenBox, baconBox, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
