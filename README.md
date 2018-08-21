11c34d899a9205bf5e64b214c4052da3b28ec984
= <a.widzinski@gmail.com>
2018-08-21 21:35:57 +0200

    add listener to choiceBox

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This is my project for training writing JavaFX applications.
-### This project display simple window with choiceBox.
+### This project shows how to implement listener on choiceBox.
 
 ### This project is based on tutorial from youtube, link below:
 https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,7 +3,6 @@ package sample;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -16,7 +15,6 @@ public class Main extends Application {
 
     private Stage window;
     private Scene scene;
-    private Button button;
 
 
     @Override
@@ -27,13 +25,11 @@ public class Main extends Application {
         ChoiceBox<String> meets = new ChoiceBox<>();
         meets.getItems().addAll("Bacon", "Chicken", "Ham");
         meets.setValue("Bacon");
-
-        button = new Button("Click me");
-        button.setOnAction(event -> System.out.println(meets.getValue()));
+        meets.setOnAction(event -> System.out.println(meets.getValue()));
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll(meets, button);
+        layout.getChildren().addAll(meets);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
