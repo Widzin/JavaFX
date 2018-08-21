9b180a310f71737bc650c7026c150152eb0b0ffb
= <a.widzinski@gmail.com>
2018-08-21 23:11:43 +0200

    create treeView

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,27 +2,50 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private Stage window;
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    private Stage window;
+    private TreeView<String> treeView;
+    private TreeItem<String> marvel, hulk, thor;
 
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
 
+        marvel = new TreeItem<>("Marvel");
+        marvel.setExpanded(true);
+
+        hulk = makeBranch("Hulk", marvel);
+        makeBranch("Bruce", hulk);
+        makeBranch("Banner", hulk);
+
+        thor = makeBranch("Thor", marvel);
+        makeBranch("Odinson", thor);
+
+        treeView = new TreeView<>(marvel);
+        treeView.setShowRoot(true);
+
         StackPane layout = new StackPane();
+        layout.getChildren().add(treeView);
         Scene scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    private TreeItem<String> makeBranch(String text, TreeItem<String> parent) {
+        TreeItem<String> children = new TreeItem<>(text);
+        children.setExpanded(true);
+        parent.getChildren().add(children);
+        return children;
     }
 }
