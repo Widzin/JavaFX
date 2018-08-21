1764835aa05eb0c4faa58d508597eb1d07a8c3be
= <a.widzinski@gmail.com>
2018-08-21 22:37:42 +0200

    added combobox

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,18 +17,26 @@ public class Main extends Application {
     private Stage window;
     private Scene scene;
     private Button button;
-
+    private ComboBox<String> comboBox;
 
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
-
         button = new Button("Click me");
+
+        comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(
+                "Matrix",
+                "Shrek",
+                "Red Riding Hood",
+                "Zootopia"
+        );
+        comboBox.setPromptText("Choose a movie");
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll( button);
+        layout.getChildren().addAll(comboBox, button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
