c486f05ff7b8b289b8497058bc0853d99505b51d
Artur Widziński <a.widzinski@gmail.com>
2018-11-09 22:43:33 +0100

    simplify Main class, add line to .gitignore to not add .jar files

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,8 +3,6 @@ package sample;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,19 +14,14 @@ public class Main extends Application {
 
     private Stage window;
     private Scene scene;
-    private Button button;
-
 
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
 
-        button = new Button("Click me");
-
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
-        layout.getChildren().addAll( button);
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
