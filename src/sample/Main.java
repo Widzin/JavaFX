b6ecb3e1c8d94bda5eb63384c18de816b417a1a4
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 18:32:43 +0100

    use IntegerProperties and bind them

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -18,13 +19,22 @@ public class Main extends Application {
     private Scene scene;
     private Button button;
 
-
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
 
+        IntegerProperty x = new SimpleIntegerProperty(5);
+        IntegerProperty y = new SimpleIntegerProperty();
+
+        y.bind(x.multiply(10));
+
         button = new Button("Click me");
+        button.setOnAction(e -> {
+            x.setValue(x.getValue() + 2);
+            System.out.println("X: " + x.getValue());
+            System.out.println("Y: " + y.getValue());
+        });
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20));
