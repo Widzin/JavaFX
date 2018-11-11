b8b50a6ea5a53d2ed3b7d3b7e5b7f3a0cbd4671f
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 20:17:04 +0100

    use Controller to handle view events

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public Button btn;
+
+    public void clickBtn() {
+        System.out.println("The button has been clicked.");
+        btn.setText("Stop click me.");
+    }
 }
