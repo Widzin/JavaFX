83fc86ff8cb1b58fbc5843b52afbad2c2b138a9f
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 18:40:44 +0100

    create dynamic gui

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,12 +1,14 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
@@ -16,19 +18,25 @@ public class Main extends Application {
 
     private Stage window;
     private Scene scene;
-    private Button button;
-
 
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
 
-        button = new Button("Click me");
+        TextField userInput = new TextField();
+        userInput.setMaxWidth(200);
 
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20));
-        layout.getChildren().addAll( button);
+        Label firstLabel = new Label("Welcome to the site ");
+        Label secondLabel = new Label();
+
+        HBox bottomText = new HBox(firstLabel, secondLabel);
+        bottomText.setAlignment(Pos.CENTER);
+
+        VBox layout = new VBox(10, userInput, bottomText);
+        layout.setAlignment(Pos.CENTER);
+
+        secondLabel.textProperty().bind(userInput.textProperty());
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
