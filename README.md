d2b28ab5e50dc0ede9875a65a621e506df6425aa
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 17:13:37 +0100

    add new menu with radio checked options

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # This is my project for training writing JavaFX applications.
 ### This project display simple window with menu bars.
+### This project is based on lessons from 21 to 24.
 
 ### This project is based on tutorial from youtube, link below:
 https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,12 +20,14 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
-
         layout = new BorderPane();
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(prepareFileMenu(), prepareEditMenu(), prepareHelpMenu());
+        menuBar.getMenus()
+                .addAll(prepareFileMenu(),
+                        prepareEditMenu(),
+                        prepareHelpMenu(),
+                        prepareDifficultMenu());
         layout.setTop(menuBar);
-
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
@@ -33,37 +35,30 @@ public class Main extends Application {
 
     private Menu prepareFileMenu() {
         Menu fileMenu = new Menu("_File");
-
         MenuItem newItem = new MenuItem("New...");
         newItem.setOnAction(e -> System.out.println("Create a new file..."));
         fileMenu.getItems().add(newItem);
-
         fileMenu.getItems().add(new MenuItem("Open..."));
         fileMenu.getItems().add(new MenuItem("Save..."));
         fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Settings..."));
         fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Exit"));
-
         return fileMenu;
     }
 
     private Menu prepareEditMenu() {
         Menu editMenu = new Menu("_Edit");
-
         editMenu.getItems().add(new MenuItem("Cut"));
         editMenu.getItems().add(new MenuItem("Copy"));
-
         MenuItem pasteItem = new MenuItem("Paste");
         pasteItem.setDisable(true);
         editMenu.getItems().add(pasteItem);
-
         return editMenu;
     }
 
     private Menu prepareHelpMenu() {
         Menu helpMenu = new Menu("Help");
-
         CheckMenuItem showLines = new CheckMenuItem("Show line Numbers");
         showLines.setOnAction(e -> {
             if (showLines.isSelected())
@@ -71,12 +66,22 @@ public class Main extends Application {
             else
                 System.out.println("Program won't now display line numbers");
         });
-
         CheckMenuItem autoSave = new CheckMenuItem("Enable autosave");
         autoSave.setSelected(true);
-
         helpMenu.getItems().addAll(showLines, autoSave);
-
         return helpMenu;
+    }
+
+    private Menu prepareDifficultMenu() {
+        Menu difficultMenu = new Menu("Difficulty");
+        ToggleGroup difficultyToggle = new ToggleGroup();
+        RadioMenuItem easy = new RadioMenuItem("Easy");
+        RadioMenuItem medium = new RadioMenuItem("Medium");
+        RadioMenuItem hard = new RadioMenuItem("Hard");
+        easy.setToggleGroup(difficultyToggle);
+        medium.setToggleGroup(difficultyToggle);
+        hard.setToggleGroup(difficultyToggle);
+        difficultMenu.getItems().addAll(easy, medium, hard);
+        return difficultMenu;
     }
 }
