90399faa16533a5b4ba73ac002326c9bc04d4e59
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 21:08:55 +0100

    use scene builder to create gui without writing code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,4 +11,8 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Loading user data...");
     }
+
+    public void loginBtnClick() {
+        System.out.println("You are logged in!");
+    }
 }
