fc5401c45b2cf08c7428b424f640052471294999
Artur Widziński <a.widzinski@gmail.com>
2018-11-11 13:09:49 +0100

    create simple menu

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,17 +16,31 @@ public class Main extends Application {
 
     private Stage window;
     private Scene scene;
+    private BorderPane layout;
 
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setTitle("Hello World");
 
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20));
+        layout = new BorderPane();
+        prepareFileMenu();
 
         scene = new Scene(layout, 300, 250);
         window.setScene(scene);
         window.show();
+    }
+
+    private void prepareFileMenu() {
+        Menu fileMenu = new Menu("File");
+        fileMenu.getItems().add(new MenuItem("New file"));
+        fileMenu.getItems().add(new MenuItem("Open file"));
+        fileMenu.getItems().add(new MenuItem("Save file"));
+        fileMenu.getItems().add(new MenuItem("Exit"));
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(fileMenu);
+
+        layout.setTop(menuBar);
     }
 }
